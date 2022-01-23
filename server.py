7611d3693d68d0c5e7d3b4a4b769c7e75b62219d
infiniteloop12 <tylercarrollengineer@gmail.com>
2022-01-23 14:36:49 -0600

    Updating to new website layout
    Updating to new website layout

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+    return render_template("index.html")
+
+@app.route("/engineeringresume")
+def engineering_resume_page():
+    return render_template("engineering_resume.html")
+
+@app.route("/codingresume")
+def coding_resume_page():
+    return render_template("coding_resume.html")
+
+@app.route("/thanks")
+def thanks_page():
+    print("thanks!")
+    return render_template("thanks.html.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
